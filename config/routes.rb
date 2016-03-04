74194213bad614647b2a1c633be3a172d5f02e62
Scarlett Wu <swu0207@berkeley.edu>
2016-03-03 19:26:39 -0800

    I'M DONE

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get "users", to: "users#index"
-  get "users/new", to: "users#new"
-  get "users/:id", to: "users#show", as: :user
-  post "users", to: "users#create"
+  get "dogs", to: "dogs#index"
+  get "dogs/new", to: "dogs#new"
+  get "dogs/:id", to: "dogs#show", as: :dog
+  post "dogs", to: "dogs#create"
+
+  # get "users", to: "users#index"
+  # get "users/new", to: "users#new"
+  # get "users/:id", to: "users#show", as: :user
+  # post "users", to: "users#create"
 end
